72fcd22237dedeaff0ae771196ae165bc2d245fa
Bruce Smith <b.smith@us.imshealth.com>
2019-07-20 15:40:22 -0400

    playing around

--- main.go
+++ main.go
@@ -1,147 +1,182 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"os/exec"
 	"time"
 )
 
+const (
+	vmadmPath = "/usr/sbin/vmadm"
+)
+
 func main() {
-	fmt.Println("vim-go")
+	vm := VMADMGet(&VMADMGetInput{
+		UUID: "256729f4-9974-49c4-961c-36eb95042ed1",
+	})
+	fmt.Printf("%+v\n", vm)
+
+}
+
+func VMADMGet(input *VMADMGetInput) *VM {
+	cmd := exec.Command("vmadm", "get", input.UUID)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var vm VM
+	err = json.Unmarshal(stdout.Bytes(), &vm)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return &vm
+}
+
+type VMADMGetInput struct {
+	UUID string `json:"uuid"`
 }
 
 type VM struct {
-	Alias                      string            `json:"alias"`
-	ArchiveOnDelete            bool              `json:"archive_on_delete"`
-	AutoBoot                   bool              `json:"auto_boot"`
-	BillingID                  string            `json:"billing_id"`
-	BhyveExtraOpts             string            `json:"bhyve_extra_opts"`
-	Boot                       string            `json:"boot"`
-	BootTimestamp              time.Time         `json:"boot_time_stamp"`
-	Bootrom                    string            `json:"bootrom"`
-	Brand                      string            `json:"brand"`
-	CPUCap                     int               `json:"cpu_cap"`
-	CPUShares                  int               `json:"cpu_shares"`
-	CPUType                    string            `json:"cpu_type"`
-	CreateTimestamp            time.Time         `json:"create_timestamp"`
-	ServerUUID                 string            `json:"server_uuid"`
-	CustomerMetadata           map[string]string `json:"customer_metadata"`
-	Datasets                   string            `json:"datasets"`
-	DelegateDataset            bool              `json:"delegate_dataset"`
-	Disks                      []Disk            `json:"disks"`
-	DiskDriver                 string            `json:"disk_driver"`
-	DoNotInventory             bool              `json:"do_not_inventory"`
-	DNSDomain                  string            `json:"dns_domain"`
-	FileSystems                []FileSystem      `json:"filesystems"`
-	FirewallEnabled            bool              `json:"firewall_enabled"`
-	FlexibleDiskSize           int               `json:"flexible_disk_size"`
-	FSAllowed                  string            `json:"fs_allowed"`
-	Hostname                   string            `json:"hostname"`
-	HVM                        bool              `json:"hvm"`
-	ImageUUID                  string            `json:"image_uuid"`
-	InternalMetadata           map[string]string `json:"internal_metadata"`
-	InternalMetadataNamespaces []string          `json:"internal_metadata_namespaces"`
-	IndestructibleDelegated    bool              `json:"indestructible_delegated"`
-	IndestructibleZoneRoot     string            `json:"indestructible_zoneroot"`
-	KernelVersion              string            `json:"kernel_version"`
-	LimitPriv                  []string          `json:"limit_priv"`
-	MaintainResolvers          bool              `json:"maintain_resolvers"`
-	MaxLockedMemory            int               `json:"max_locked_memory"`
-	MaxLWPS                    int               `json:"max_lwps"`
-	MaxPhysicalMemory          int               `json:"max_physical_memory"`
-	MaxSwap                    int               `json:"max_swap"`
-	MdataExecTimeout           int               `json:"mdata_exec_timeout"`
-	NICs                       []NIC             `json:"nics"`
-	NICDriver                  string            `json:"nic_driver"`
-	NoWait                     bool              `json:"nowait"`
-	OwnerUUID                  string            `json:"owner_uuid"`
-	PackageName                string            `json:"package_name"`
-	PackageVersion             string            `json:"package_version"`
-	PID                        int               `json:"pid"`
-	QEMUOpts                   []string          `json:"qemu_opts"`
-	QEMUExtraOpts              []string          `json:"qemu_extra_opts"`
-	Quota                      int               `json:"quota"`
-	RAM                        int               `json:"ram"`
-	Resolvers                  []string          `json:"resolvers"`
-	Routes                     map[string]string `json:"routes"`
-	Snapshots                  []string          `json:"snapshots"`
-	SpiceOpts                  string            `json:"spice_opts"`
-	SpicePassword              string            `json:"spice_password"`
-	SpicePort                  string            `json:"spice_port"`
-	State                      string            `json:"state"`
-	TMPFS                      string            `json:"tmpfs"`
-	TransitionExpire           time.Time         `json:"transition_expire"`
-	TransitionTo               string            `json:"transition_to"`
-	Type                       string            `json:"type"`
-	UUID                       string            `json:"uuid"`
-	VCPUs                      int               `json:"vcpus"`
-	VGA                        string            `json:"vga"`
-	VirtIOTXBurst              int               `json:"virtio_txburst"`
-	VirtIOTXTimer              int               `json:"virtio_txtimer"`
-	VNCPassword                string            `json:"vnc_password"`
-	VNCPort                    int               `json:"vnc_port"`
-	ZFSDataCompression         string            `json:"zfs_data_compression"`
-	ZFSDataRecSize             int               `json:"zfs_data_recsize"`
-	ZFSFilesystemLimit         int               `json:"zfs_filesystem_limit"`
-	ZFSIOPriority              int               `json:"zfs_io_priority"`
-	ZFSRootCompression         string            `json:"zfs_root_compression"`
-	ZFSRootRecSize             int               `json:"zfs_root_recsize"`
-	ZFSSnapshotLimit           int               `json:"zfs_snapshot_limit"`
-	ZLogMaxSize                int               `json:"zlog_max_size"`
-	ZLogMode                   string            `json:"zlog_mode"`
-	ZoneState                  string            `json:"zone_state"`
-	ZonePath                   string            `json:"zonepath"`
-	ZoneName                   string            `json:"zonename"`
-	ZoneDID                    int               `json:"zonedid"`
-	ZoneID                     int               `json:"zoneid"`
-	ZPool                      string            `json:"zpool"`
+	Alias                      string            `json:"alias", omitempty"`
+	ArchiveOnDelete            bool              `json:"archive_on_delete", omitempty"`
+	AutoBoot                   bool              `json:"auto_boot", omitempty"`
+	BillingID                  string            `json:"billing_id", omitempty"`
+	BhyveExtraOpts             string            `json:"bhyve_extra_opts", omitempty"`
+	Boot                       string            `json:"boot", omitempty"`
+	BootTimestamp              time.Time         `json:"boot_time_stamp", omitempty"`
+	Bootrom                    string            `json:"bootrom", omitempty"`
+	Brand                      string            `json:"brand", omitempty"`
+	CPUCap                     int               `json:"cpu_cap", omitempty"`
+	CPUShares                  int               `json:"cpu_shares", omitempty"`
+	CPUType                    string            `json:"cpu_type", omitempty"`
+	CreateTimestamp            time.Time         `json:"create_timestamp", omitempty"`
+	ServerUUID                 string            `json:"server_uuid", omitempty"`
+	CustomerMetadata           map[string]string `json:"customer_metadata", omitempty"`
+	Datasets                   string            `json:"datasets", omitempty"`
+	DelegateDataset            bool              `json:"delegate_dataset", omitempty"`
+	Disks                      []Disk            `json:"disks", omitempty"`
+	DiskDriver                 string            `json:"disk_driver", omitempty"`
+	DoNotInventory             bool              `json:"do_not_inventory", omitempty"`
+	DNSDomain                  string            `json:"dns_domain", omitempty"`
+	FileSystems                []FileSystem      `json:"filesystems", omitempty"`
+	FirewallEnabled            bool              `json:"firewall_enabled", omitempty"`
+	FlexibleDiskSize           int               `json:"flexible_disk_size", omitempty"`
+	FSAllowed                  string            `json:"fs_allowed", omitempty"`
+	Hostname                   string            `json:"hostname", omitempty"`
+	HVM                        bool              `json:"hvm", omitempty"`
+	ImageUUID                  string            `json:"image_uuid", omitempty"`
+	InternalMetadata           map[string]string `json:"internal_metadata", omitempty"`
+	InternalMetadataNamespaces []string          `json:"internal_metadata_namespaces", omitempty"`
+	IndestructibleDelegated    bool              `json:"indestructible_delegated", omitempty"`
+	IndestructibleZoneRoot     string            `json:"indestructible_zoneroot", omitempty"`
+	KernelVersion              string            `json:"kernel_version", omitempty"`
+	LimitPriv                  string            `json:"limit_priv", omitempty"`
+	MaintainResolvers          bool              `json:"maintain_resolvers", omitempty"`
+	MaxLockedMemory            int               `json:"max_locked_memory", omitempty"`
+	MaxLWPS                    int               `json:"max_lwps", omitempty"`
+	MaxPhysicalMemory          int               `json:"max_physical_memory", omitempty"`
+	MaxSwap                    int               `json:"max_swap", omitempty"`
+	MdataExecTimeout           int               `json:"mdata_exec_timeout", omitempty"`
+	NICs                       []NIC             `json:"nics", omitempty"`
+	NICDriver                  string            `json:"nic_driver", omitempty"`
+	NoWait                     bool              `json:"nowait", omitempty"`
+	OwnerUUID                  string            `json:"owner_uuid", omitempty"`
+	PackageName                string            `json:"package_name", omitempty"`
+	PackageVersion             string            `json:"package_version", omitempty"`
+	PID                        int               `json:"pid", omitempty"`
+	QEMUOpts                   []string          `json:"qemu_opts", omitempty"`
+	QEMUExtraOpts              []string          `json:"qemu_extra_opts", omitempty"`
+	Quota                      int               `json:"quota", omitempty"`
+	RAM                        int               `json:"ram", omitempty"`
+	Resolvers                  []string          `json:"resolvers", omitempty"`
+	Routes                     map[string]string `json:"routes", omitempty"`
+	Snapshots                  []string          `json:"snapshots", omitempty"`
+	SpiceOpts                  string            `json:"spice_opts", omitempty"`
+	SpicePassword              string            `json:"spice_password", omitempty"`
+	SpicePort                  string            `json:"spice_port", omitempty"`
+	State                      string            `json:"state", omitempty"`
+	TMPFS                      int               `json:"tmpfs", omitempty"`
+	TransitionExpire           time.Time         `json:"transition_expire", omitempty"`
+	TransitionTo               string            `json:"transition_to", omitempty"`
+	Type                       string            `json:"type", omitempty"`
+	UUID                       string            `json:"uuid", omitempty"`
+	VCPUs                      int               `json:"vcpus", omitempty"`
+	VGA                        string            `json:"vga", omitempty"`
+	VirtIOTXBurst              int               `json:"virtio_txburst", omitempty"`
+	VirtIOTXTimer              int               `json:"virtio_txtimer", omitempty"`
+	VNCPassword                string            `json:"vnc_password", omitempty"`
+	VNCPort                    int               `json:"vnc_port", omitempty"`
+	ZFSDataCompression         string            `json:"zfs_data_compression", omitempty"`
+	ZFSDataRecSize             int               `json:"zfs_data_recsize", omitempty"`
+	ZFSFilesystemLimit         int               `json:"zfs_filesystem_limit", omitempty"`
+	ZFSIOPriority              int               `json:"zfs_io_priority", omitempty"`
+	ZFSRootCompression         string            `json:"zfs_root_compression", omitempty"`
+	ZFSRootRecSize             int               `json:"zfs_root_recsize", omitempty"`
+	ZFSSnapshotLimit           int               `json:"zfs_snapshot_limit", omitempty"`
+	ZLogMaxSize                int               `json:"zlog_max_size", omitempty"`
+	ZLogMode                   string            `json:"zlog_mode", omitempty"`
+	ZoneState                  string            `json:"zone_state", omitempty"`
+	ZonePath                   string            `json:"zonepath", omitempty"`
+	ZoneName                   string            `json:"zonename", omitempty"`
+	ZoneDID                    int               `json:"zonedid", omitempty"`
+	ZoneID                     int               `json:"zoneid", omitempty"`
+	ZPool                      string            `json:"zpool", omitempty"`
 }
 
 type Disk struct {
-	BlockSize      int    `json:"block_size"`
-	Boot           bool   `json:"boot"`
-	Compression    string `json:"compression"`
-	NoCreate       bool   `json:"nocreate"`
-	ImageName      string `json:"image_name"`
-	ImageSize      int    `json:"image_size"`
-	ImageUUID      string `json:"image_uuid"`
-	PCISlot        string `json:"pci_slot"`
-	RefReservation string `json:"refreservation"`
-	Size           int    `json:"size"`
-	Media          string `json:"media"`
-	Model          string `json:"model"`
-	ZPool          string `json:"zpool"`
+	BlockSize      int    `json:"block_size", omitempty"`
+	Boot           bool   `json:"boot", omitempty"`
+	Compression    string `json:"compression", omitempty"`
+	NoCreate       bool   `json:"nocreate", omitempty"`
+	ImageName      string `json:"image_name", omitempty"`
+	ImageSize      int    `json:"image_size", omitempty"`
+	ImageUUID      string `json:"image_uuid", omitempty"`
+	PCISlot        string `json:"pci_slot", omitempty"`
+	RefReservation string `json:"refreservation", omitempty"`
+	Size           int    `json:"size", omitempty"`
+	Media          string `json:"media", omitempty"`
+	Model          string `json:"model", omitempty"`
+	ZPool          string `json:"zpool", omitempty"`
 }
 
 type FileSystem struct {
-	Type    string   `json:"type"`
-	Source  string   `json:"source"`
-	Target  string   `json:"target"`
-	Raw     string   `json:"raw"`
-	Options []string `json:"options"`
+	Type    string   `json:"type", omitempty"`
+	Source  string   `json:"source", omitempty"`
+	Target  string   `json:"target", omitempty"`
+	Raw     string   `json:"raw", omitempty"`
+	Options []string `json:"options", omitempty"`
 }
 
 type NIC struct {
-	AllowIPSpoofing        bool     `json:"allow_ip_spoofing"`
-	AllowMACSpoofing       bool     `json:"allow_mac_spoofing"`
-	AllowRestrictedTraffic bool     `json:"allow_restricted_traffic"`
-	AllowUnfilteredPromisc bool     `json:"allow_unfiltered_promisc"`
-	BlockedOutgoingPorts   []int    `json:"blocked_outgoing_ports"`
-	AllowedIPs             []string `json:"allowed_ips"`
-	AllowedDHCPCIDs        []string `json:"allowed_dhcp_cids"`
-	DHCPServer             bool     `json:"dhcp_server"`
-	Gateway                string   `json:"gateway"`
-	Gateways               []string `json:"gateways"`
-	Interface              string   `json:"interface"`
-	IP                     string   `json:"ip"`
-	IPs                    []string `json:"ips"`
-	MAC                    string   `json:"mac"`
-	Model                  string   `json:"model"`
-	MTU                    int      `json:"mtu"`
-	Netmask                string   `json:"netmask"`
-	NetworkUUID            string   `json:"network_uuid"`
-	NICTag                 string   `json:"nic_tag"`
-	Primary                bool     `json:"primary"`
-	VLANIDs                int      `json:"vlan_ids"`
-	VRRPPrimaryIP          string   `json:"vrrp_primary_ip"`
-	VRRPVRID               int      `json:"vrrp_vrid"`
+	AllowIPSpoofing        bool     `json:"allow_ip_spoofing", omitempty"`
+	AllowMACSpoofing       bool     `json:"allow_mac_spoofing", omitempty"`
+	AllowRestrictedTraffic bool     `json:"allow_restricted_traffic", omitempty"`
+	AllowUnfilteredPromisc bool     `json:"allow_unfiltered_promisc", omitempty"`
+	BlockedOutgoingPorts   []int    `json:"blocked_outgoing_ports", omitempty"`
+	AllowedIPs             []string `json:"allowed_ips", omitempty"`
+	AllowedDHCPCIDs        []string `json:"allowed_dhcp_cids", omitempty"`
+	DHCPServer             bool     `json:"dhcp_server", omitempty"`
+	Gateway                string   `json:"gateway", omitempty"`
+	Gateways               []string `json:"gateways", omitempty"`
+	Interface              string   `json:"interface", omitempty"`
+	IP                     string   `json:"ip", omitempty"`
+	IPs                    []string `json:"ips", omitempty"`
+	MAC                    string   `json:"mac", omitempty"`
+	Model                  string   `json:"model", omitempty"`
+	MTU                    int      `json:"mtu", omitempty"`
+	Netmask                string   `json:"netmask", omitempty"`
+	NetworkUUID            string   `json:"network_uuid", omitempty"`
+	NICTag                 string   `json:"nic_tag", omitempty"`
+	Primary                bool     `json:"primary", omitempty"`
+	VLANIDs                int      `json:"vlan_ids", omitempty"`
+	VRRPPrimaryIP          string   `json:"vrrp_primary_ip", omitempty"`
+	VRRPVRID               int      `json:"vrrp_vrid", omitempty"`
 }
